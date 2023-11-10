3372e2ea22753f7fe83af07a4150555675a72cb3
Marianameji <97916603+Marianameji@users.noreply.github.com>
2023-11-10 09:17:27 -0500

    versión 1.0
    primera versión del proyecto.

---
+++ config/conexion.php
@@ -0,0 +1,10 @@
+<? 
+$host="bpwdwmscxjzz8rxuar2w-mysql.services.clever-cloud.com";
+$user="uzqxexmtabajwvat";
+$pwd="m7UoyZL7sStKv4XhNINW";
+$bd="bpwdwmscxjzz8rxuar2w";
+$con=mysqli_connect($host,$user,$pwd,$bd)
+or die ("problemas con la conexión");
+
+$con->close();
+?>
